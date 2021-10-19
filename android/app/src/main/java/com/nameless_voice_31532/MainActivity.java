456a1657474f3154bfe106b34f1d1387b05f5763
Crowdbotics <team@crowdbotics.com>
2021-10-19 00:18:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nameless_voice_31532/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nameless_voice_31532;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nameless_voice_31532";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nameless_voice_31532",
+  url: "https://nameless_voice_31532.botics.co",
+  api: "https://nameless_voice_31532.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
